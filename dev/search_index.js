6f2436b39f0de1ae3aae87a195a7bcf8d59078e3
Documenter.jl <documenter@juliadocs.github.io>
2023-01-22 13:06:59 +0000

    build based on f9eed05

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = XKeyboard","category":"page"},{"location":"#XKeyboard","page":"Home","title":"XKeyboard","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for XKeyboard.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [XKeyboard]","category":"page"}]
+}
